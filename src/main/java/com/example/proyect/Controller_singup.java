04879eda619337b7ca15f019b0de96b0fee5063d
Davidfer <davidfernando1112@gmail.com>
2023-02-28 11:04:19 -0500

    Restricciones espacios y visibilidad contraseña

--- src/main/java/com/example/proyect/Controller_singup.java
+++ src/main/java/com/example/proyect/Controller_singup.java
@@ -1,4 +1,75 @@
 package com.example.proyect;
 
-public class Controller_singup {
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller_singup implements Initializable {
+
+
+    @FXML
+    private TextField txtEmailSingup;
+    @FXML
+    private TextField txtNumeroSingup;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtConfirmPassword;
+    @FXML
+    private ComboBox<String> cbCountry;
+    @FXML
+    private Button btnSingUp;
+    @FXML
+    private Button btnCleanSingUp;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+        // metodos para restringir el uso de espacios en contraseñas y usuario
+        txtPassword.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCharacter().equals(" ")){
+                    event.consume();
+                }
+            }
+        });
+        txtConfirmPassword.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCharacter().equals(" ")){
+                    event.consume();
+                }
+            }
+        });
+        txtEmailSingup.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCharacter().equals(" ")){
+                    event.consume();
+                }
+            }
+        });
+        txtNumeroSingup.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCharacter().equals(" ")){
+                    event.consume();
+                }
+            }
+        });
+
+    }
+
+
 }
